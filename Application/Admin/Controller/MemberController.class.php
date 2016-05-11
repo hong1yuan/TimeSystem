a55e8d33496050f8d62ca478324d9de46c1645ca
hong1yuan <hong1yuan@126.com>
2016-05-11 15:44:12 +0800

    密码修改

--- Application/Admin/Controller/MemberController.class.php
+++ Application/Admin/Controller/MemberController.class.php
@@ -3,25 +3,76 @@ namespace Admin\Controller;
 use Think\Controller;
 class MemberController extends Controller {
 
-    /**
-     * 会员管理
-     */
-    public function member(){
-        $this->display();
-    }
+  
     /**
      * 修改资料
      */
     public function profile(){
+
+        // $user = session("userid");
+       
+      $user = 1000;
+        $value = M('member') -> field('reid,rename,username,telephone,ulevel,alipay,bankuser,bankname,bankcard') -> where("id = '$user'") -> find();
+
+        $this -> assign('value',$value);
+
+
         $this->display();
+    }
+
+    /**
+     *更新资料
+     */
+    public function updata(){
+
+      $user = 1000;
+      $arry["alipay"] = I('post.iemail');
+      $arry["bankname"] = I('post.ibankname');
+      $arry["bankuser"] = I('post.ibankuser');
+      $arry["bankcard"] = I('post.ibankcard');
+      
+      $value = M('member');
+
+      $value -> where("id = '$user'") ->save($arry);
+      $this -> redirect('profile');
+   
+
+
+
     }
 
     /**
      * 修改密码
      */
     public function password(){
+        $user = 1000;
+        $value = M('member') -> field('username') -> where("id = '$user'") ->find();
+        $this -> assign('value',$value);
+
         $this->display();
     }
+
+    /*
+      修改密码逻辑
+     */
+
+    public function passwordup(){
+             $user = 1000;
+             $password =   substr(md5(trim($_POST['password'])),8,16);
+             $value = M('member');
+             $uname = $value -> where("id = '$user' AND password ='$password'") ->select();
+
+             if($uname == false){
+              $this -> error('密码错误');
+              $this -> redirect('password');
+             }
+
+             $arryn["password"] =  substr(md5(trim($_POST['newpassword'])),8,16); 
+             
+             $value -> where("id = '$user'") ->save($arryn);
+             $this -> success('修改成功',U('profile'));
+
+        }
 
     /**
      * 注册会员
@@ -34,8 +85,15 @@ class MemberController extends Controller {
      * 激活账号
      */
     public function activate(){
+        $this->display();
 
     }
-
+    
+    /*
+     * 团队结构
+     */
+    public function teamt(){
+        $this->display();
+    }
 
 }
