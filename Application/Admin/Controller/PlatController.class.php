5b17c9a22abf0f61f52cd32ddf9a8007eb2afac9
IIVVTT <IIVVTT@qq.com>
2016-05-13 15:30:39 +0800

    平台

--- Application/Admin/Controller/PlatController.class.php
+++ Application/Admin/Controller/PlatController.class.php
@@ -6,8 +6,22 @@ class PlatController extends Controller {
      * 交易平台设置
      */
     public function index(){
-        
+        $config = M('webconfig')->where("id = 1")->find();
+        $telephone = M('Member')->where('id=1000')->getField('telephone');
+        $this->assign('telephone',$telephone);
+        $this->assign('config',$config);
         $this->display();
+    }
+
+    public function webxiugai(){
+        $arr=$_POST;
+       // $arr['fenhong']
+        $result = M('webconfig')->where("id=1")->save($arr);
+        if($result){
+            $this->success('修改成功',U('index'));
+        }else{
+            $this->error('修改失败',U('index'));
+        }
     }
 
 }
